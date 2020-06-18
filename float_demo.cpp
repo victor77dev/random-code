006dcc44458a81a3d10217e2cbcf3559c8d4f6a6
victor77dev <victor77.dev@gmail.com>
2020-06-18 23:51:25 +0200

    Floating points demo class template

---
+++ float_demo.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+class float_demo_8 {
+    char num[2];
+    int exp; // 5 bits
+    int frac; // 11 bits
+
+    void setExpFrac(string input) {
+        exp = 0;
+        frac = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+
+        }
+    }
+
+    public:
+    float_demo_8(string input) {
+    }
+    float_demo_8(): float_demo_8(""){}
+
+    float_demo_8& operator=(string input) {
+
+        return *this;
+    }
+
+    char firstChar() {
+        return num[0];
+    }
+
+    char secondChar() {
+        return num[1];
+    }
+
+    string getBits() {
+        bitset<8> first(num[0]);
+        bitset<8> second(num[1]);
+
+        return first.to_string() + second.to_string();
+    }
+};
+
+
+int main() {
+    float_demo_8 f;
+    f = "0.1";
+    printf("%c%c\n", f.firstChar(), f.secondChar());
+    cout << f.getBits() << endl;
+}
